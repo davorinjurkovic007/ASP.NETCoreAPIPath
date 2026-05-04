ef9140be9a09b6074bdf4788c26f11eb877803d9
Davorin <davorin.jurkovic@gmail.com>
2026-05-04 23:10:29 +0200

    -- full example of how to use TailwindCSS in ASP.NET Core

--- FullStackSolution/TailwindMVC/TailwindMVC/Program.cs
+++ FullStackSolution/TailwindMVC/TailwindMVC/Program.cs
@@ -1,6 +1,9 @@
 // How to Integrate Tailwind CSS on an ASP.NET MVC Web Application
 // https://youtu.be/6xcusQFp9EU?si=3JrHlAekrG-S38tA
 
+// Get started with Tailwind CSS
+// https://tailwindcss.com/docs/installation/tailwind-cli
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
