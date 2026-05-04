b083ff6209eb17574cd0387e8df5af5898d5605d
Davorin <davorin.jurkovic@gmail.com>
2026-05-04 22:36:17 +0200

    Created basic home controller and basic action and staff

--- FullStackSolution/TailwindMVC/TailwindMVC/Program.cs
+++ FullStackSolution/TailwindMVC/TailwindMVC/Program.cs
@@ -1,6 +1,14 @@
+// How to Integrate Tailwind CSS on an ASP.NET MVC Web Application
+// https://youtu.be/6xcusQFp9EU?si=3JrHlAekrG-S38tA
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
+
 
 app.Run();
